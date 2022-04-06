9ad1a5b01c22d8263761f67b7a265083445450b0
myxsan <yksl3461@gmail.com>
2022-04-06 04:49:22 +0300

    Set up the growing system

--- Snake-Clone-GAME/Assets/Scripts/Snake.cs
+++ Snake-Clone-GAME/Assets/Scripts/Snake.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Snake : MonoBehaviour
 {
     private Vector2 _direction = Vector2.right;
+    private List<Transform> _segments;
+    public Transform segmentPrefab;
+
+    private void Start() 
+    {
+        _segments = new List<Transform>();
+        _segments.Add (this.transform);    
+    }
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.W))
@@ -24,12 +32,31 @@ public class Snake : MonoBehaviour
             _direction = Vector2.right;
         }
     }
-    private void FixedUpdate()
+    private async void FixedUpdate()
     {
+        for (int i = _segments.Count -1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+
         this.transform.position = new Vector3(
             Mathf.Round(this.transform.position.x) + _direction.x,
             Mathf.Round(this.transform.position.y) + _direction.y,
             0.0f
         );
+    }
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add(segment);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Food")
+        {
+            Grow();
+        }
     }
 }
